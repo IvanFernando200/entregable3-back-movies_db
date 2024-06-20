5806190b8b9fe26e32d5d1695c59ea4a78901c9b
Ivan <125326960+IvanFernando200@users.noreply.github.com>
2024-06-20 05:59:03 -0500

    movies_db first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const actorRouter = require("./actor.router");
+const directorRouter = require("./director.router");
+const movieRouter = require("./movie.router");
+const genreRouter = require("./genre.router");
 
 // colocar las rutas aquí
-
+router.use(actorRouter);
+router.use(directorRouter);
+router.use(genreRouter);
+router.use(movieRouter);
 
 module.exports = router;
